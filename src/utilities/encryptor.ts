56b97f35cff04eb36941586566d221d04caca8b5
Yaroslav Surilov <ysurilov@lohika.com>
2021-07-28 16:13:54 +0300

    update deps, implement basic Encryptor

--- src/utilities/encryptor.ts
+++ src/utilities/encryptor.ts
@@ -1,18 +1,31 @@
 // built-ins
-import crypto from 'crypto';
+import crypto, { Cipher } from 'crypto';
 
 // interfaces
 import { Encryptor as EncryptorInterface } from '../interfaces/encryptor';
 
 
 export class Encryptor implements EncryptorInterface {
+    private readonly algorithm: string;
     private readonly secretKey: string;
+    private readonly initVector: Buffer;
+
+    constructor () {
+        // relatively fast, because of using parallelization
+        this.algorithm = 'aes-256-ctr';
+        this.secretKey = 'fZp8FHoBKaSlFzbq2mJMyEjBQHL19gy2';
+        this.initVector = crypto.randomBytes(16);
+    }
 
     encrypt ( data: string ): string {
-        return data;
+        const cipher: Cipher = crypto.createCipheriv(this.algorithm, this.secretKey, this.initVector);
+
+        return cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
     }
 
     decrypt ( data: string ): string {
-        return data;
+        const decipher: Cipher = crypto.createDecipheriv(this.algorithm, this.secretKey, this.initVector);
+
+        return decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
     }
 }
